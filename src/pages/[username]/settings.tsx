75136f7bd27f5c7b752d8ed9f9bc76625e50376b
takenoko <takenoko.tech@gmail.com>
2021-08-10 00:13:15 +0900

    fix view.

--- src/pages/[username]/settings.tsx
+++ src/pages/[username]/settings.tsx
@@ -1,23 +1,44 @@
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType, NextPage, NextPageContext } from 'next';
 import Layout from '../../components/layout';
 import Head from 'next/head';
 import utilStyles from '../../styles/utils.module.css';
 import { useRouter } from 'next/router';
+import React from 'react';
+import useSWR from 'swr';
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  const json = await res.json();
+  return json['']['text'];
+};
 
 export default function Post(props: InferGetStaticPropsType<typeof getStaticProps>) {
-  console.log(useRouter().query);
   const username = props.username;
+  const res1 = useSWR('/api/hello', fetcher);
+  const res2 = useSWR('/api/hello', fetcher);
+
+  console.log(useRouter().query);
+  console.log(res1);
+  console.log(res2);
+
+  const error = res1.error || res2.error ? <div>failed to load</div> : null;
+  const loading = !res1.data || !res2.data ? <div>loading...</div> : null;
+
   return (
     <Layout>
       <Head>
         <title>{username}</title>
       </Head>
       <h1 className={utilStyles.headingXl}>{username}</h1>
+      {error || loading}
+      {res1.data ? <div>{res1.data}</div> : null}
+      {res2.data ? <div>{res2.data}</div> : null}
     </Layout>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = async (context) => {
+  console.log('getStaticPaths');
   return {
     paths: [],
     fallback: true,
@@ -25,6 +46,7 @@ export const getStaticPaths: GetStaticPaths = async (context) => {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
+  console.log('getStaticProps');
   return {
     props: {
       username: context.params.username,
--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,6 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.method);
+  await sleep(3);
   res.status(200).json({ text: 'Hello' });
+}
+
+function sleep(sec: number) {
+  return new Promise((resolve) => setTimeout(resolve, sec * 1000));
 }
