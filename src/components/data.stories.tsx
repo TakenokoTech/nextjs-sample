4a064bfafe0ca7561be53ab27d7430aa96540a2c
takenoko <takenoko.tech@gmail.com>
2021-08-11 00:45:56 +0900

    add stories.

---
+++ src/components/data.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Date from './date';
+
+export default {
+  title: 'Date',
+};
+
+export const show = () => <Date dateString='2020-01-01' />;
---
+++ src/components/layout.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from './layout';
+
+export default {
+  title: 'Layout',
+};
+
+export const show = () => <Layout />;
