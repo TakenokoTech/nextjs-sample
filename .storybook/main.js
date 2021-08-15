6bdb2ee032230ce17063d78ec1f093990a75d219
takenoko <takenoko.tech@gmail.com>
2021-08-15 17:28:07 +0900

    fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,36 +1,21 @@
 const loaders = (options = {}) => [
-  { loader: 'style-loader' },
-  {
-    loader: 'css-loader',
-    options: {
-      importLoaders: 1,
-      ...options,
-    },
-  },
-  { loader: 'postcss-loader' },
+  { loader: 'style-loader' }, //
+  { loader: 'css-loader', options: { importLoaders: 1, ...options } }, //
+  { loader: 'postcss-loader' }, //
 ];
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-
-  // Custom
-  webpackFinal: async (config) => ({
-    ...config,
-    module: {
-      ...config.module,
-      rules: config.module.rules.reduce(
-        (acc, cur) => [
-          ...acc,
-          ...(cur.test.toString() === /\.css$/.toString()
-            ? [
-                { test: /\.module\.css$/, use: loaders({ modules: true }) },
-                { test: /^.*(?<!\.module)\.css$/, use: loaders() },
-              ]
-            : [cur]),
-        ],
-        [],
-      ),
-    },
-  }),
+  webpackFinal: async (config) => {
+    config.module.rules = [
+      // デフォルトのrulesに入っているCSS用の設定は、「.module.css」とバッティングするため除外
+      ...config.module.rules.filter((rule) => rule.test.source !== /\.css$/.source),
+      //「.module.css」の設定
+      { test: /\.module\.css$/, use: loaders({ modules: true }) },
+      //「.css」の設定
+      { test: /^.*(?<!\.module)\.css$/, use: loaders() },
+    ];
+    return config;
+  },
 };
